7440428d1c36ff7ff68d73f50a247c24b2b4f67e
sunshanghai <sunshanghai@baidu.com>
2017-01-09 20:42:05 +0800

    init

---
+++ app/src/main/java/com/sshsun/cnbusinessmagazine/activity/HomePageActivity.java
@@ -0,0 +1,18 @@
+package com.sshsun.cnbusinessmagazine.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sshsun.cnbusinessmagazine.R;
+
+/**
+ * Created by shsun on 17/1/9.
+ */
+
+public class HomePageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
